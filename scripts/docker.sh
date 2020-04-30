53b9784e794a6e591cad623462c253b5a93a3b62
j-griffith <john.griffith8@gmail.com>
2020-04-30 13:43:32 -0600

    update docker group settings

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -4,4 +4,4 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 apt update -y
 apt install -y docker-ce
-usermod -aG docker ${USER}
+usermod -aG docker ubuntu
